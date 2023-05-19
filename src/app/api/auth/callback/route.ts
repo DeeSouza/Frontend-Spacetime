27fb1d39df9122eb8ec4cf5589d7345020c8e6a1
Diego Souza <dee-souza@hotmail.com>
2023-05-19 10:48:46 -0300

    create middleware and route memories new and page

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,14 +5,12 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
-  const registerResponse = await api.post('/register', {
-    code,
-  })
+  const redirectTo = request.cookies.get('redirectTo')?.value
+  const registerResponse = await api.post('/register', { code })
+  const redirectUrl = redirectTo ?? new URL('/', request.url)
 
   const { token } = registerResponse.data
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30
-
-  const redirectUrl = new URL('/', request.url)
 
   return NextResponse.redirect(redirectUrl, {
     headers: {
