fd03e894ad4caa894c18e0cfb92d9dfe911fa9ef
Diego Souza <dee-souza@hotmail.com>
2023-05-19 16:09:46 -0300

    list memories home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
+import { cookies } from 'next/headers'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { api } from '@/lib/api'
+import { MemoryCard } from '@/components/MemoryCard'
 
-export default function Home() {
-  return <EmptyMemories />
+interface Memory {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+
+  const response = await api.get('/memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => (
+        <MemoryCard key={memory.id} memory={memory} />
+      ))}
+    </div>
+  )
 }
